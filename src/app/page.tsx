ccd5169fa41d29fa725ed4b030cb2701a0e76fe9
grigri201 <144085936+grigri201@users.noreply.github.com>
2025-06-14 20:31:40 +0800

    Add MarkdownSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
+'use client';
+
+import MarkdownSection from '@/components/MarkdownSection';
+
 export default function Home() {
   return (
     <div className="relative min-h-screen p-4">
-      <div
-        contentEditable
-        suppressContentEditableWarning
-        className="border p-4 min-h-[200px]"
-      >
-        Editable content
-      </div>
-      <div className="fixed bottom-2 left-1/2 -translate-x-1/2 flex w-[80%] items-center">
-        <input
-          type="text"
-          className="flex-grow bg-transparent border rounded p-2"
-        />
-        <button className="ml-2 bg-blue-600 text-white rounded px-4 py-2">
-          Send
-        </button>
-      </div>
+      <MarkdownSection content="Editable content" />
     </div>
   );
 }
---
+++ src/components/MarkdownSection.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState, useRef } from "react";
+import { marked } from "marked";
+
+interface MarkdownSectionProps {
+  content: string;
+}
+
+export default function MarkdownSection({ content }: MarkdownSectionProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState(content);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+    // focus the div after state update
+    setTimeout(() => {
+      divRef.current?.focus();
+    });
+  };
+
+  const handleBlur = () => {
+    if (divRef.current) {
+      setText(divRef.current.innerText);
+    }
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="relative">
+      <div
+        ref={divRef}
+        contentEditable={isEditing}
+        suppressContentEditableWarning
+        onDoubleClick={handleDoubleClick}
+        onBlur={handleBlur}
+        className="border p-4 min-h-[200px] outline-none"
+        dangerouslySetInnerHTML={!isEditing ? { __html: marked.parse(text) } : undefined}
+      >
+        {isEditing ? text : undefined}
+      </div>
+      {isEditing && (
+        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 flex w-[80%] items-center">
+          <input type="text" className="flex-grow bg-transparent border rounded p-2" />
+          <button className="ml-2 bg-blue-600 text-white rounded px-4 py-2">Send</button>
+        </div>
+      )}
+    </div>
+  );
+}
